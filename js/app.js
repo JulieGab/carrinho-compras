d15411480a447db52a1e375ed6cd9c218ace5699
JulieGab <jullygabriella.sa@gmail.com>
2024-01-29 21:40:28 -0300

    feat: adicionando o produto no carrinho

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ function adicionar (){
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario =  produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade')
-    alert(nomeProduto);
-    alert(produto);
-    alert(quantidade.value);
-    let preco = quantidade.value * valorUnitario;
-    alert(preco);
-    // calcular o preço, o subtotal
+    let quantidade = document.getElementById('quantidade').value;
+    // calcular o preço, nosso subtotal
+    let preco = quantidade * valorUnitario;
     // adicionar no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$1400</span>
+  </section>`
     // atualizar o valor total da compra
 
     }
