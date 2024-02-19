e0541287dfdf507edd4027f55d6e7a7250777bd5
JulieGab <jullygabriella.sa@gmail.com>
2024-02-19 16:51:43 -0300

    feat: finalizando projeto alura

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
-function adicionar (){
-    // recuperar valores do nome do produto e quantidade
+let totalGeral;
+limpar();
+
+function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
-    let valorUnitario =  produto.split('R$')[1];
+    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
-    // calcular o preço, nosso subtotal
     let preco = quantidade * valorUnitario;
-    // adicionar no carrinho
+
     let carrinho = document.getElementById('lista-produtos');
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$1400</span>
-  </section>`
-    // atualizar o valor total da compra
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
+  </section>`;
 
-    }
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
+}
 
 function limpar() {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0';
 }
